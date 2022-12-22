740610671de3dcc336857247df0d4a5f8d40936f
Edye Quisca <edye.1230@gmail.com>
2022-12-21 20:29:58 -0500

    game v2

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 document.getElementById("bolita").addEventListener("click", sumarPuntos);
 
 let puntos = 0;
-let num1, num2;
-let tiempo = 60;
+let num1, num2, num3, num4;
+
 function sumarPuntos() {
     puntos++;
     document.getElementById("puntos").innerHTML = `Puntos: ${puntos}`;
     num1 = Math.round(Math.random() * 450);
     num2 = Math.round(Math.random() * 450);
+    num3 = Math.round(Math.random() * 450);
+    num4 = Math.round(Math.random() * 450);
 
     document.getElementById("bolita").style.marginTop = num1 + "px";
     document.getElementById("bolita").style.marginLeft = num2 + "px";
 
+
+
+  let newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "minas");
+    newDiv.style.marginTop = num3 + "px";
+    newDiv.style.marginLeft = num4 + "px";
+
+    document.getElementById('contenedor_xx').appendChild(newDiv);  
 };
 
 function restarTiempo() {
@@ -21,5 +31,24 @@ function restarTiempo() {
         clearInterval(tiempoInterval);
         clearInterval(puntosInterval);
         alert("Perdiste");
-    }
+    };
 };
+
+document.getElementById("bolita").addEventListener("mouseleave", movimiento);
+
+function restablecer() {
+    document.getElementById("bolita").style.backgroundColor = "red";
+}
+
+function movimiento() {
+
+    if (puntos > 0 ) document.getElementById("minas").addEventListener("click", perdida);
+
+    function perdida(){
+        alert("Perdiste");
+        location.reload();
+    }
+
+    if (puntos === 100 ) alert("Ganaste");
+
+}
